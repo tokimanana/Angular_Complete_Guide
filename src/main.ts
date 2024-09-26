60ecb850bfe8920c05115f0d82b8ab41b415cc99
tokimanana <tokimananasarobidy@gmail.com>
2024-09-27 01:30:43 +0400

    Version 1 resumed

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 
-import { AppComponent } from './app/AppComponent';
+import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent).catch((err) => console.error(err));
