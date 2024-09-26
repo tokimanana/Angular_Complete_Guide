f8af71e3e496d2e1a04ad4980287e37a5e279f30
tokimanana <tokimananasarobidy@gmail.com>
2024-09-26 22:21:31 +0400

    Version 1

---
+++ src/app/AppComponent.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { DUMMY_USERS } from './dummy-users';
+import { HeaderComponent } from './header/header.component';
+import { TasksComponent } from './tasks/tasks.component';
+import { UserComponent } from './user/user.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [HeaderComponent, UserComponent, TasksComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+export class AppComponent {
+  users = DUMMY_USERS;
+  selectedUserId = 'u1';
+
+  get selectedUser() {
+    return this.users.find((user) => user.id === this.selectedUserId);
+  }
+
+  onSelectUser(id: string) {
+    this.selectedUserId = id;
+  }
+}
