0dc72a5024175ad85594d1194a94c323dc52307d
tokimanana <tokimananasarobidy@gmail.com>
2024-09-27 12:35:32 +0400

    Complete Button setup

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,10 +1,6 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 
-type User = {
-  id: string;
-  avatar:string;
-  name: string;
-}
+import { type User } from './user.model';
 
 @Component({
   selector: 'app-user',
@@ -12,11 +8,9 @@ type User = {
   templateUrl: './user.component.html',
   styleUrl: './user.component.css',
 })
-
 export class UserComponent {
   @Input({ required: true }) user!: User;
   @Output() select = new EventEmitter<string>();
-
 
   get imagePath() {
     return 'assets/users/' + this.user.avatar;
