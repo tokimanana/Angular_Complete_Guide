98785da46c677368dc17259fc9883035d2aa9bed
tokimanana <tokimananasarobidy@gmail.com>
2024-09-27 15:45:56 +0400

    Create New Task

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input } from '@angular/core';
+
 import { TaskComponent } from './task/task.component';
-import { NewTaskComponent } from "./new-task/new-task.component";
+import { NewTaskComponent } from './new-task/new-task.component';
+import { type NewTaskData } from './task/task.model';
 
 @Component({
   selector: 'app-tasks',
@@ -49,11 +51,22 @@ export class TasksComponent {
     this.tasks = this.tasks.filter((task) => task.id !== id);
   }
 
-  onStartAddTask(){
+  onStartAddTask() {
     this.isAddingTask = true;
   }
 
-  onCancelAddTask(){
+  onCancelAddTask() {
+    this.isAddingTask = false;
+  }
+
+  onAddTask(taskData: NewTaskData) {
+    this.tasks.push({
+      id: new Date().getTime().toString(),
+      userId: this.userId,
+      title: taskData.title,
+      summary: taskData.summary,
+      dueDate: taskData.date,
+    });
     this.isAddingTask = false;
   }
 }
