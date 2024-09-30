b19d1d846de32aa3d6695d82ca763a0b1ffe07b5
tokimanana <tokimananasarobidy@gmail.com>
2024-09-30 09:29:41 +0400

    2-Task services

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -1,4 +1,5 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { TasksService } from './../tasks.service';
+import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { type NewTaskData } from '../task/task.model';
 
@@ -10,21 +11,27 @@ import { type NewTaskData } from '../task/task.model';
   styleUrl: './new-task.component.css',
 })
 export class NewTaskComponent {
-  @Output() cancel = new EventEmitter();
-  @Output() add = new EventEmitter<NewTaskData>();
+  @Input({ required: true }) userId!: string;
+  @Output() close = new EventEmitter();
+
   enteredTitle = '';
   enteredSummary = '';
   enteredDate = '';
+  private TasksService = inject(TasksService);
 
   onCancel() {
-    this.cancel.emit();
+    this.close.emit();
   }
 
   onSubmit() {
-    this.add.emit({
-      title: this.enteredTitle,
-      summary: this.enteredSummary,
-      date: this.enteredDate,
-    });
+    this.TasksService.addTask(
+      {
+        title: this.enteredTitle,
+        summary: this.enteredSummary,
+        date: this.enteredDate,
+      },
+      this.userId
+    );
+    this.close.emit();
   }
 }
