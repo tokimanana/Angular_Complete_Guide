a0b73aa7c3d472b4e8adbb42eec02cdfe89099af
tokimanana <tokimananasarobidy@gmail.com>
2024-09-30 10:39:32 +0400

    Button Grey

--- src/app/tasks/task/task.component.ts
+++ src/app/tasks/task/task.component.ts
@@ -1,24 +1,26 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 
-import { type Task } from './task.model'
-import { CardComponent } from "../../shared/card/card.component";
-import { DatePipe } from '@angular/common';
+import { type Task } from './task.model';
+import { CardComponent } from '../../shared/card/card.component';
+import { DatePipe, CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-task',
   standalone: true,
-  imports: [CardComponent, DatePipe],
+  imports: [CardComponent, DatePipe, CommonModule],
   templateUrl: './task.component.html',
   styleUrl: './task.component.css',
 })
 export class TaskComponent {
-  @Input({required : true}) task!: Task;
+  @Input({ required: true }) task!: Task;
   @Output() complete = new EventEmitter<string>();
 
-  onCompleteTask(){
+  onCompleteTask() {
     // this.complete.emit(this.task.id);
-    console.log("completed");
+
     console.log(this.task);
     this.task.completed = true;
+    console.log("Class applied:", this.task.completed);
   }
 }
