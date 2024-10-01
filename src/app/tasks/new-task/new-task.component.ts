40bf95a6429f2127f34864d01460b9854436818c
tokimanana <tokimananasarobidy@gmail.com>
2024-10-01 11:16:16 +0400

    Form Validation

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -1,12 +1,13 @@
-import { TasksService } from './../tasks.service';
 import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { type NewTaskData } from '../task/task.model';
+import { AbstractControl, FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
+import { TasksService } from './../tasks.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-new-task',
   standalone: true,
-  imports: [FormsModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './new-task.component.html',
   styleUrl: './new-task.component.css',
 })
@@ -14,24 +15,31 @@ export class NewTaskComponent {
   @Input({ required: true }) userId!: string;
   @Output() close = new EventEmitter();
 
-  enteredTitle = '';
-  enteredSummary = '';
-  enteredDate = '';
+  taskForm: FormGroup;
   private TasksService = inject(TasksService);
+
+  constructor (private fb: FormBuilder) {
+    this.taskForm = this.fb.group({
+      title: ['', [Validators.required, Validators.minLength(3)]],
+      summary: ['', [Validators.required, Validators.minLength(10)]],
+      date: ['', [Validators.required, this.futureDateValidator]],
+    })
+  }
+
+  futureDateValidator(control: AbstractControl): { [key: string]: boolean} | null {
+    const currentDate = new Date();
+    const selectedDate = new Date(control.value);
+    return selectedDate > currentDate ? null : {'invalidDate' : true};
+  }
 
   onCancel() {
     this.close.emit();
   }
 
   onSubmit() {
-    this.TasksService.addTask(
-      {
-        title: this.enteredTitle,
-        summary: this.enteredSummary,
-        date: this.enteredDate,
-      },
-      this.userId
-    );
+    if(this.taskForm.valid){
+      this.TasksService.addTask(this.taskForm.value, this.userId);
+    }
     this.close.emit();
   }
 }
